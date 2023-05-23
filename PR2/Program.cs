c8bc41e6c4553420c36b22742ff0668418490c78
rusuFely <felyvic02@gmail.com>
2023-05-23 10:48:31 +0300

    Add project files.

---
+++ PR2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PRlab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string multicastIP = "239.5.6.7";
+            int port = 5002;
+
+            UDPChat chat = new UDPChat(multicastIP, port);
+
+            chat.StartReceiveLoop();
+
+            Console.WriteLine("Input format: <IP>:<TEXT>");
+            Console.WriteLine("IP=0 - MULTICAST");
+            //adaugat try, catch 
+            while (true)
+            {
+                var input = Console.ReadLine() ?? "";
+                var splitted = input.Split(':');
+                var toIP = splitted[0];
+                var text = splitted[1];
+                
+                if (toIP != "0")
+                {
+                    chat.SendTo(toIP, text);
+                }
+                else
+                {
+                    chat.SendGeneral(text);
+                }
+            }
+        }
+    }
+}
---
+++ PR2/UDPChat.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace PRlab2
+{
+    public class UDPChat
+    {
+        private int _port;
+        private string _multicastIP;
+        private Socket _multicastSocket;
+        private Socket _senderSocket;
+
+        public UDPChat(string multicastIP, int port)
+        {
+            _port = port;
+            _multicastIP = multicastIP;
+
+            var hostIP = new IPEndPoint(IPAddress.Any, port);
+            _multicastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            _multicastSocket.Bind(hostIP);
+            _multicastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
+                 new MulticastOption(IPAddress.Parse(multicastIP), IPAddress.Any));
+
+            _senderSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            _senderSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership,
+                 new MulticastOption(IPAddress.Parse(multicastIP), IPAddress.Any));
+
+            _senderSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 50);
+        }
+
+        public void StartReceiveLoop()
+        {
+            Thread thread = new Thread(new ThreadStart(receive));
+
+            thread.Start();
+        }
+
+        public void SendTo(string ip, string text)
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), _port);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            _senderSocket.SendTo(bytes, remoteEndPoint);
+        }
+
+        public void SendGeneral(string text)
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(_multicastIP), _port);
+
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            _senderSocket.SendTo(bytes, remoteEndPoint);
+        }
+
+        private void receive()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                EndPoint remoteClient = new IPEndPoint(0, 0);
+
+                _multicastSocket.ReceiveFrom(buffer, ref remoteClient);
+                string text = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"Received from: {remoteClient} : {text}");
+            }
+        }
+
+    }
+}
